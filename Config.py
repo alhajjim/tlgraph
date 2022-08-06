5e4f2417d084f5944bd890b9433cf38741891a83
AHMED DR <110259797+alhajjim@users.noreply.github.com>
2022-08-06 20:34:28 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,15 +4,11 @@ ENVIRONMENT = bool(os.environ.get('ENVIRONMENT', False))
 
 if ENVIRONMENT:
     try:
-        API_ID = int(os.environ.get('API_ID', 0))
-    except ValueError:
-        raise Exception("Your API_ID is not a valid integer.")
+    API_ID = int(os.environ.get('API_ID', None))
     API_HASH = os.environ.get('API_HASH', None)
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
     try:
-        OWNER_ID = int(os.environ.get('OWNER_ID', "5297963487"))
-    except ValueError:
-        raise Exception("Your OWNER_ID is not a valid integer.")
+    OWNER_ID = int(os.environ.get('OWNER_ID', None))
     DATABASE_URL = os.environ.get('DATABASE_URL', None)
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
@@ -21,9 +17,9 @@ if ENVIRONMENT:
         MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
     # Fill the Values
-    API_ID = 0
+    API_ID = ""
     API_HASH = ""
     BOT_TOKEN = ""
     DATABASE_URL = ""
     MUST_JOIN = ""
-    OWNER_ID = 0
+    OWNER_ID = ""
